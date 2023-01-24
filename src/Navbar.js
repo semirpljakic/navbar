e9c77da1509eeaf5edd27aef50e2b191d0a007fc
Semir Pljakić <93542041+semirpljakic@users.noreply.github.com>
2023-01-24 09:30:04 +0100

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
             const { id, url, icon } = socialy;
             return (
               <li key={id}>
-                <a href={url}>{icon}</a>
+                <a href={url} target='_blank'>{icon}</a>
               </li>
             );
           })}
